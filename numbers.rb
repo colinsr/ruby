b8a5f51f570876f86b71d0f6aec4b084d114eff3
Colin Detwiller <colinsr@me.com>
2015-10-29 19:58:08 -0400

    playing with numbers in ruby

---
+++ numbers.rb
@@ -0,0 +1,23 @@
+puts 1 + 2
+
+a = 1
+b = 2
+puts a + b
+
+c = a + b
+d = c - a
+
+puts d
+
+number = 1
+decimal = 1.1
+puts number.class
+puts decimal.class
+mix = number + decimal
+puts mix
+puts mix.class
+
+name = "Colin"
+
+# this doesn't work
+# puts number + name
