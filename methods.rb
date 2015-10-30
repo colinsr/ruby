9834621f12034a45f3c725251d39af16f174f565
Colin Detwiller <colinsr@me.com>
2015-10-29 20:36:32 -0400

    wrapping up basic ruby methods

--- methods.rb
+++ methods.rb
@@ -21,3 +21,17 @@ end
 third_method(2)
 
 fourth_method(2,3)
+
+def fifth(a,b)
+  puts "Adding #{a} and #{b};"
+  return a + b
+end
+
+puts fifth(7,12)
+
+# cool feature of Ruby interactive
+# in order to load a file into the interactive
+# start irb
+# then type:
+# load "./filename.rb"
+# then you'll have access to any of the methods declared in the file.
